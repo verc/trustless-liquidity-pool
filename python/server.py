3ec3f7b83cef16c3dce63570c1977b47ec8d5388
doetsch <patrick.doetsch@gmail.com>
2015-03-15 01:05:22 +0100

    deviation fix

--- python/server.py
+++ python/server.py
@@ -24,7 +24,7 @@ _interest = { 'poloniex' : { 'btc' : { 'rate' : 0.00165, 'target' : 50.0, 'fee' 
             }
 _nuconfig = '%s/.nu/nu.conf'%os.getenv("HOME") # path to nu.conf
 _tolerance = 0.0075 # price tolerance
-_sampling = 12 # number of requests validated per minute
+_sampling = 24 # number of requests validated per minute
 _autopayout = True # try to send payouts automatically
 _minpayout = 0.011 # minimum balance to trigger payout
 _grantaddress = "" # custodian grant address
