aa90d5369342380579917602b3da1cc46254ffdf
Patrick Doetsch <patrick.doetsch@i6.informatik.rwth-aachen.de>
2015-03-16 21:09:18 +0100

    windows beta .bat file

---
+++ python/beta.bat
@@ -0,0 +1,14 @@
+@echo off
+SET users=users.dat
+if not exist users.dat (
+  if exist users.txt (
+    SET users=users.txt
+  ) else (
+    @echo on
+    echo error: You must specify a file called users.txt with your exchange data
+    pause
+    exit
+  )
+)
+@echo on
+client.py 104.245.36.10:2020 %users%
