a29eec144663b90dd119b57229e58a8691124dc2
doetsch <patrick.doetsch@gmail.com>
2015-03-23 15:30:21 +0100

    client tweaks

--- python/trading.py
+++ python/trading.py
@@ -241,7 +241,7 @@ class PyBot(ConnectionThread):
                     self.logger.info('increasing tier 1 %s limit of unit %s on %s from %.2f to %.2f',
                       side, self.unit, repr(self.exchange), weight + self.limit[side], weight + info['target'] - mass)
                     self.limit[side] = info['target'] - mass
-                  elif weight >= 2.0 and weight - contrib >= 1.0 and contrib / weight < 0.9:
+                  elif weight >= 2.0 and weight - contrib >= 1.0 and contrib / weight < 0.85:
                     self.logger.info('decreasing tier 1 %s limit of unit %s on %s from %.2f to %.2f',
                       side, self.unit, repr(self.exchange), weight + self.limit[side], max(0.5, contrib))
                     self.cancel_orders(side)
