2cd794763929bebfe8168b213477a9ee4998e743
doetsch <patrick.doetsch@gmail.com>
2015-03-24 05:25:46 +0100

    server fix and trading update

--- python/trading.py
+++ python/trading.py
@@ -245,9 +245,9 @@ class PyBot(ConnectionThread):
                     self.limit[side] = info['target'] - mass
                   elif weight >= 2.0 and weight - contrib > 0.1 * info['target'] and contrib / weight < 0.80:
                     self.logger.info('decreasing tier 1 %s limit of unit %s on %s from %.2f to %.2f',
-                      side, self.unit, repr(self.exchange), weight + self.limit[side], max(0.5, contrib))
+                      side, self.unit, repr(self.exchange), weight + self.limit[side], max(0.5, weight * 0.9))
                     self.cancel_orders(side)
-                    self.limit[side] = max(0.5, contrib) # place at least 0.5 NBT to see when interest raises again
+                    self.limit[side] = max(0.5, weight * 0.9) # place at least 0.5 NBT to see when interest raises again
                   elif self.limit[side] == 0 and contrib / weight > 0.95:
                     step = max(0.5, contrib * 0.1)
                     self.logger.info('increasing tier 1 %s limit of unit %s on %s from %.2f to %.2f',
