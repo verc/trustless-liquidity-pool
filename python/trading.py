2d4646c2975ee2864550558eb66a00c3bd48ed73
doetsch <patrick.doetsch@gmail.com>
2015-03-24 15:40:33 +0100

    client tading fix that can lead to non terminating client

--- python/trading.py
+++ python/trading.py
@@ -205,62 +205,58 @@ class PyBot(ConnectionThread):
     curtime = time.time()
     efftime = time.time()
     lasttime = time.time()
-    try:
-      while self.active:
-        time.sleep(max(1 - time.time() + curtime, 0))
-        curtime = time.time()
-        if curtime - lasttime < 30: continue
-        lasttime = curtime
-        if self.requester.errorflag:
-          self.shutdown()
-        else:
-          response = self.conn.get('price/' + self.unit, trials = 3, timeout = 15)
-          if not 'error' in response:
-            self.serverprice = response['price']
-            userprice = PyBot.pricefeed.price(self.unit)
-            if 1.0 - min(self.serverprice, userprice) / max(self.serverprice, userprice) > 0.005: # validate server price
-              self.logger.error('server price %.8f for unit %s deviates too much from price %.8f received from ticker, will delete all orders for this unit', self.serverprice, self.unit, userprice)
-              self.shutdown()
-            else:
-              deviation = 1.0 - min(prevprice, self.serverprice) / max(prevprice, self.serverprice)
-              if deviation > 0.00375:
-                self.logger.info('price of unit %s moved from %.8f to %.8f, will try to reset orders', self.unit, prevprice, self.serverprice)
-                prevprice = self.serverprice
-                self.cancel_orders()
-              elif curtime - efftime > 120:
-                efftime = curtime
-                for side in [ 'bid', 'ask' ]:
-                  info = self.requester.interest()[side]
-                  if 'orders' in info and len(info['orders']) > 0:
-                    orders = {}
-                    for sample in info['orders']:
-                      for order in sample:
-                        orders[order['id']] = (order['amount'], order['cost'])
-                    weight = sum([ orders[o][0] for o in orders if o in self.orders and orders[o][1] <= info['target'] ]) #sum([order['amount'] for order in info['orders'][-1] if order['id'] in self.orders])
-                    mass = sum([ orders[o][0] for o in orders ]) #sum([order['amount'] for order in info['orders'][-1]])
-                    contrib = sum([ orders[o][0] for o in orders if o in self.orders and orders[o][1] >= self.requester.cost[side] ])
-                    #print "weight:", weight, "mass:", mass, "contrib:", contrib, "sel:", len(orders.keys()), "info:", len(info['orders'])
-                    if mass + self.limit[side] < info['target']:
-                      self.logger.info('increasing tier 1 %s limit of unit %s on %s from %.2f to %.2f',
-                        side, self.unit, repr(self.exchange), weight + self.limit[side], weight + info['target'] - mass)
-                      self.limit[side] = info['target'] - mass
-                    elif weight >= 2.0 and weight - contrib > 0.1 * info['target'] and contrib / weight < 0.80:
-                      self.logger.info('decreasing tier 1 %s limit of unit %s on %s from %.2f to %.2f',
-                        side, self.unit, repr(self.exchange), weight + self.limit[side], max(0.5, weight * 0.9))
-                      self.cancel_orders(side)
-                      self.limit[side] = max(0.5, weight * 0.9) # place at least 0.5 NBT to see when interest raises again
-                    elif self.limit[side] == 0 and (weight == 0 or contrib / weight > 0.95):
-                      step = max(0.5, contrib * 0.1)
-                      self.logger.info('increasing tier 1 %s limit of unit %s on %s from %.2f to %.2f',
-                        side, self.unit, repr(self.exchange), weight + self.limit[side], weight + self.limit[side] + step)
-                      self.limit[side] += step
-              self.place('bid')
-              self.place('ask')
+    while self.active:
+      time.sleep(max(1 - time.time() + curtime, 0))
+      curtime = time.time()
+      if curtime - lasttime < 30: continue
+      lasttime = curtime
+      if self.requester.errorflag:
+        self.shutdown()
+      else:
+        response = self.conn.get('price/' + self.unit, trials = 3, timeout = 15)
+        if not 'error' in response:
+          self.serverprice = response['price']
+          userprice = PyBot.pricefeed.price(self.unit)
+          if 1.0 - min(self.serverprice, userprice) / max(self.serverprice, userprice) > 0.005: # validate server price
+            self.logger.error('server price %.8f for unit %s deviates too much from price %.8f received from ticker, will delete all orders for this unit', self.serverprice, self.unit, userprice)
+            self.shutdown()
           else:
-            self.logger.error('unable to retrieve server price: %s', response['message'])
-    except: pass
-    try:
-      time.sleep(1) # this is to ensure that the order book is updated
-      self.shutdown()
-    except: pass
+            deviation = 1.0 - min(prevprice, self.serverprice) / max(prevprice, self.serverprice)
+            if deviation > 0.00375:
+              self.logger.info('price of unit %s moved from %.8f to %.8f, will try to reset orders', self.unit, prevprice, self.serverprice)
+              prevprice = self.serverprice
+              self.cancel_orders()
+            elif curtime - efftime > 120:
+              efftime = curtime
+              for side in [ 'bid', 'ask' ]:
+                info = self.requester.interest()[side]
+                if 'orders' in info and len(info['orders']) > 0:
+                  orders = {}
+                  for sample in info['orders']:
+                    for order in sample:
+                      orders[order['id']] = (order['amount'], order['cost'])
+                  weight = sum([ orders[o][0] for o in orders if o in self.orders and orders[o][1] <= info['target'] ]) #sum([order['amount'] for order in info['orders'][-1] if order['id'] in self.orders])
+                  mass = sum([ orders[o][0] for o in orders ]) #sum([order['amount'] for order in info['orders'][-1]])
+                  contrib = sum([ orders[o][0] for o in orders if o in self.orders and orders[o][1] >= self.requester.cost[side] ])
+                  #print "weight:", weight, "mass:", mass, "contrib:", contrib, "sel:", len(orders.keys()), "info:", len(info['orders'])
+                  if mass + self.limit[side] < info['target']:
+                    self.logger.info('increasing tier 1 %s limit of unit %s on %s from %.2f to %.2f',
+                      side, self.unit, repr(self.exchange), weight + self.limit[side], weight + info['target'] - mass)
+                    self.limit[side] = info['target'] - mass
+                  elif weight >= 2.0 and weight - contrib > 0.1 * info['target'] and contrib / weight < 0.80:
+                    self.logger.info('decreasing tier 1 %s limit of unit %s on %s from %.2f to %.2f',
+                      side, self.unit, repr(self.exchange), weight + self.limit[side], max(0.5, weight * 0.9))
+                    self.cancel_orders(side)
+                    self.limit[side] = max(0.5, weight * 0.9) # place at least 0.5 NBT to see when interest raises again
+                  elif self.limit[side] == 0 and (weight == 0 or contrib / weight > 0.95):
+                    step = max(0.5, contrib * 0.1)
+                    self.logger.info('increasing tier 1 %s limit of unit %s on %s from %.2f to %.2f',
+                      side, self.unit, repr(self.exchange), weight + self.limit[side], weight + self.limit[side] + step)
+                    self.limit[side] += step
+            self.place('bid')
+            self.place('ask')
+        else:
+          self.logger.error('unable to retrieve server price: %s', response['message'])
+    time.sleep(1) # this is to ensure that the order book is updated
+    self.shutdown()
     self.release()
