2a7073d84fab813d81254e402f216bb3a6babd74
doetsch <patrick.doetsch@gmail.com>
2015-03-27 01:20:19 +0100

    timeout in validation

--- python/server.py
+++ python/server.py
@@ -312,7 +312,6 @@ def credit():
           mass = sum([orders[i][1][1] for i in xrange(len(orders)) if i == 0 or orders[i][1][0] != orders[i - 1][1][0]])
           residual = min(config._interest[name][unit][side]['target'], mass - config._interest[name][unit][side]['target'])
           weight = { user : sum([o[1][1] for o in orders if o[0] == user]) for user in users }
-          print mass, residual, weight
           if residual > 0:
             for i in xrange(len(orders)):
               user, order = orders[i]
