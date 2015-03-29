755ce4420284cecb110428da83533e842794d752
doetsch <patrick.doetsch@gmail.com>
2015-03-29 19:44:02 +0200

    server order id bug fix

--- python/server.py
+++ python/server.py
@@ -330,7 +330,7 @@ def credit():
             for user in volume[1]:
               if norm > 0 and volume[1][user] > 0:
                 price = pricelevels[lvl+1]
-                contrib = ((int(mass / target)+1) * target - mass) * volume[0][user] / norm
+                contrib = ((int(mass / target)+1) * target - mass) * volume[1][user] / norm
                 payout = contrib * price
                 volume[0][user] -= contrib
                 volume[2][user] -= contrib
