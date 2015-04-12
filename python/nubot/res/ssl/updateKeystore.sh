6d6869b9468051c70e18c8254c55c79def71a3b2
doetsch <patrick.doetsch@gmail.com>
2015-04-12 22:40:30 +0200

    removed current nubot

--- python/nubot/res/ssl/updateKeystore.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#This script downloads the latest keystore 
-#first rename existing keystore
-mv nubot_keystore.jks nubot_keystore_old.jks
-#then download from repository (branch develop)
-wget -O nubot_keystore.jks https://bitbucket.org/JordanLeePeershares/nubottrading/src/cfa0c7699ccd96300c1b1f77d416b9a6f1fa6e8d/NuBot/res/ssl/nubot_keystore.jks\?at\=develop
