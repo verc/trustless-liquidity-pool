542ab717401965053f53c84a2aa7c2f4b71efaf2
doetsch <patrick.doetsch@gmail.com>
2015-04-14 22:42:45 +0200

    pay

--- python/pay.py
+++ python/pay.py
@@ -31,25 +31,23 @@ if len(sys.argv) == 1:
 users = {}
 credits = {}
 
-#try:
-for line in open('logs/%s.log'%sys.argv[1]).readlines():
-  line = line.strip().split()
-  if line[2] == 'new':
-    if not line[7] in users:
-      users[line[7]] = []
-    users[line[7]].append(line[4])
-for line in open('logs/%s.credits'%sys.argv[1]).readlines():
-  line = line.strip().split()
-  if not line[3] in credits:
-    credits[line[3]] = 0.0
-  credits[line[3]] += float(line[2])
-#except:
-#  print >> sys.stderr, "could not read data"
-#  sys.exit(1)
+try:
+  for line in open('logs/%s.log'%sys.argv[1]).readlines():
+    line = line.strip().split()
+    if line[2] == 'new':
+      if not line[7] in users:
+        users[line[7]] = []
+      users[line[7]].append(line[4])
+  for line in open('logs/%s.credits'%sys.argv[1]).readlines():
+    line = line.strip().split()
+    if not line[3] in credits:
+      credits[line[3]] = 0.0
+    credits[line[3]] += float(line[2])
+except:
+  print >> sys.stderr, "could not read data"
+  sys.exit(1)
 
 out = {}
 for addr in users:
   out[addr] = float("%.8f" % max(sum([credits[k] for k in users[addr] if k in credits]), 0.01))
 print json.dumps(out).replace(' ', '')
-
-print users, credits
